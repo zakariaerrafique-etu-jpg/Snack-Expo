a93792842c888bfbba6ad587305819d666eae070
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 01:59:52 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
 
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+
+import AppNavigator from "./navigation/AppNavigator";
+import { RecipeProvider } from "./context/RecipeContext";
+
+export default function App() {
+  return (
+    <RecipeProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </NavigationContainer>
+    </RecipeProvider>
+  );
+}

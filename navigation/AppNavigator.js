82cc47c78aea33a147ccd3e28022a88b43659b29
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 01:58:51 +0200

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,1 +1,167 @@
 
+import React from "react";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Ionicons } from "@expo/vector-icons";
+
+import HomeScreen from "../screens/HomeScreen";
+import RecipeDetails from "../screens/RecipeDetails";
+import FavoritesScreen from "../screens/FavoritesScreen";
+import MyRecipesScreen from "../screens/MyRecipesScreen";
+import AddRecipeScreen from "../screens/AddRecipeScreen";
+import EditRecipeScreen from "../screens/EditRecipeScreen";
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+function HomeStack() {
+
+  return (
+
+    <Stack.Navigator>
+
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "Foodie"
+        }}
+      />
+
+
+      <Stack.Screen
+        name="RecipeDetails"
+        component={RecipeDetails}
+        options={{
+          title: "Détails de la recette"
+        }}
+      />
+
+
+      <Stack.Screen
+        name="AddRecipe"
+        component={AddRecipeScreen}
+        options={{
+          title: "Ajouter une recette"
+        }}
+      />
+
+
+      <Stack.Screen
+        name="EditRecipe"
+        component={EditRecipeScreen}
+        options={{
+          title: "Modifier la recette"
+        }}
+      />
+
+    </Stack.Navigator>
+
+  );
+
+}
+
+
+
+function MainTabs() {
+
+
+  return (
+
+    <Tab.Navigator
+
+      screenOptions={({route}) => ({
+
+        headerShown:false,
+
+        tabBarIcon: ({color,size}) => {
+
+
+          let iconName;
+
+
+          if(route.name === "Accueil"){
+            iconName="home";
+          }
+
+          else if(route.name==="Favoris"){
+            iconName="heart";
+          }
+
+          else if(route.name==="Mes recettes"){
+            iconName="restaurant";
+          }
+
+
+          return (
+
+            <Ionicons
+              name={iconName}
+              size={size}
+              color={color}
+            />
+
+          );
+
+        }
+
+
+      })}
+
+
+    >
+
+
+      <Tab.Screen
+        name="Accueil"
+        component={HomeStack}
+      />
+
+
+      <Tab.Screen
+        name="Favoris"
+        component={FavoritesScreen}
+      />
+
+
+      <Tab.Screen
+        name="Mes recettes"
+        component={MyRecipesScreen}
+      />
+
+
+    </Tab.Navigator>
+
+  );
+
+}
+
+
+
+export default function AppNavigator(){
+
+
+  return (
+
+    <Stack.Navigator
+      screenOptions={{
+        headerShown:false
+      }}
+    >
+
+      <Stack.Screen
+        name="Main"
+        component={MainTabs}
+      />
+
+    </Stack.Navigator>
+
+  );
+
+
+}

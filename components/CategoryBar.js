9efd8f755c7755a2ec463d4200a2204eaa9aaaca
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 02:02:32 +0200

    Update CategoryBar.js

--- components/CategoryBar.js
+++ components/CategoryBar.js
@@ -1,1 +1,74 @@
 
+import React from "react";
+
+import {
+  View,
+  FlatList,
+  StyleSheet
+} from "react-native";
+
+import CategoryItem from "./CategoryItem";
+
+
+
+export default function CategoryBar({
+  categories,
+  selectedCategory,
+  onSelectCategory
+}) {
+
+
+  return (
+
+    <View style={styles.container}>
+
+      <FlatList
+
+        data={categories}
+
+        horizontal
+
+        showsHorizontalScrollIndicator={false}
+
+        keyExtractor={(item)=>item.id}
+
+        renderItem={({item})=>(
+
+          <CategoryItem
+
+            category={item}
+
+            selected={
+              selectedCategory === item.name
+            }
+
+            onPress={()=> 
+              onSelectCategory(item.name)
+            }
+
+          />
+
+        )}
+
+      />
+
+
+    </View>
+
+  );
+
+
+}
+
+
+
+
+const styles = StyleSheet.create({
+
+  container:{
+
+    paddingVertical:10
+
+  }
+
+});

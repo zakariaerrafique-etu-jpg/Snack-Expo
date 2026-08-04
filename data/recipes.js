37ea291aec221254c2360e3de73ff00c5a7ef682
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 02:01:12 +0200

    Add recipes data to recipes.js

--- data/recipes.js
+++ data/recipes.js
@@ -1,1 +1,296 @@
 
+const recipes = [
+
+{
+id:"1",
+
+name:"Tajine marocain",
+
+category:"Marocain",
+
+image:
+"https://images.unsplash.com/photo-1547592180-85f173990554",
+
+ingredients:[
+
+"Viande",
+"Pommes de terre",
+"Carottes",
+"Oignons",
+"Épices marocaines"
+
+],
+
+instructions:[
+
+"Couper les légumes",
+
+"Faire revenir la viande avec les épices",
+
+"Ajouter les légumes",
+
+"Laisser cuire 45 minutes"
+
+],
+
+time:"45 min",
+
+servings:"4 personnes",
+
+calories:"520 kcal",
+
+difficulty:"Moyen"
+
+},
+
+
+
+{
+id:"2",
+
+name:"Pizza Margherita",
+
+category:"Pizza",
+
+image:
+"https://images.unsplash.com/photo-1565299624946-b28f40a0ae38",
+
+ingredients:[
+
+"Pâte à pizza",
+"Tomate",
+"Fromage mozzarella",
+"Basilic"
+
+],
+
+instructions:[
+
+"Préparer la pâte",
+
+"Ajouter la sauce tomate",
+
+"Ajouter le fromage",
+
+"Cuire 20 minutes"
+
+],
+
+time:"30 min",
+
+servings:"2 personnes",
+
+calories:"700 kcal",
+
+difficulty:"Facile"
+
+},
+
+
+
+{
+id:"3",
+
+name:"Salade végétarienne",
+
+category:"Végétarien",
+
+image:
+"https://images.unsplash.com/photo-1512621776951-a57141f2eefd",
+
+ingredients:[
+
+"Laitue",
+"Tomates",
+"Concombre",
+"Avocat"
+
+],
+
+instructions:[
+
+"Laver les légumes",
+
+"Couper les ingrédients",
+
+"Mélanger avec la sauce"
+
+],
+
+time:"15 min",
+
+servings:"2 personnes",
+
+calories:"250 kcal",
+
+difficulty:"Facile"
+
+},
+
+
+
+{
+id:"4",
+
+name:"Pancakes",
+
+category:"Petit déjeuner",
+
+image:
+"https://images.unsplash.com/photo-1528207776546-365bb710ee93",
+
+ingredients:[
+
+"Farine",
+"Oeufs",
+"Lait",
+"Sucre"
+
+],
+
+instructions:[
+
+"Mélanger les ingrédients",
+
+"Faire cuire dans une poêle",
+
+"Ajouter du miel"
+
+],
+
+time:"20 min",
+
+servings:"3 personnes",
+
+calories:"400 kcal",
+
+difficulty:"Facile"
+
+},
+
+
+
+{
+id:"5",
+
+name:"Burger maison",
+
+category:"Fast Food",
+
+image:
+"https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+
+ingredients:[
+
+"Pain burger",
+"Viande hachée",
+"Fromage",
+"Salade"
+
+],
+
+instructions:[
+
+"Cuisiner la viande",
+
+"Assembler le burger",
+
+"Ajouter la sauce"
+
+],
+
+time:"25 min",
+
+servings:"2 personnes",
+
+calories:"850 kcal",
+
+difficulty:"Moyen"
+
+},
+
+
+
+{
+id:"6",
+
+name:"Gâteau au chocolat",
+
+category:"Desserts",
+
+image:
+"https://images.unsplash.com/photo-1578985545062-69928b1d9587",
+
+ingredients:[
+
+"Chocolat",
+"Farine",
+"Oeufs",
+"Beurre"
+
+],
+
+instructions:[
+
+"Faire fondre le chocolat",
+
+"Mélanger les ingrédients",
+
+"Cuire au four"
+
+],
+
+time:"40 min",
+
+servings:"6 personnes",
+
+calories:"600 kcal",
+
+difficulty:"Moyen"
+
+},
+
+
+
+{
+id:"7",
+
+name:"Jus de fruits",
+
+category:"Boissons",
+
+image:
+"https://images.unsplash.com/photo-1613478223719-2ab802602423",
+
+ingredients:[
+
+"Orange",
+"Banane",
+"Fraise"
+
+],
+
+instructions:[
+
+"Couper les fruits",
+
+"Mixer les fruits",
+
+"Servir frais"
+
+],
+
+time:"10 min",
+
+servings:"2 verres",
+
+calories:"180 kcal",
+
+difficulty:"Facile"
+
+}
+
+];
+
+
+export default recipes;

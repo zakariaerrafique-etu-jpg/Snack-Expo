06a88143ecb9e165930099f515440e99504fd4ed
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 02:01:43 +0200

    Update storage.js

--- utils/storage.js
+++ utils/storage.js
@@ -1,1 +1,108 @@
 
+import AsyncStorage from 
+"@react-native-async-storage/async-storage";
+
+
+
+// Sauvegarder une donnée
+
+export const saveData = async(key,value)=>{
+
+  try{
+
+    await AsyncStorage.setItem(
+
+      key,
+
+      JSON.stringify(value)
+
+    );
+
+
+  }
+
+  catch(error){
+
+    console.log(
+      "Erreur sauvegarde : ",
+      error
+    );
+
+  }
+
+};
+
+
+
+
+
+// Récupérer une donnée
+
+export const getData = async(key)=>{
+
+  try{
+
+
+    const data = 
+
+    await AsyncStorage.getItem(key);
+
+
+
+    return data 
+    ? JSON.parse(data)
+    : null;
+
+
+  }
+
+  catch(error){
+
+    console.log(
+      "Erreur récupération : ",
+      error
+    );
+
+
+    return null;
+
+  }
+
+
+};
+
+
+
+
+
+
+// Supprimer une donnée
+
+export const removeData = async(key)=>{
+
+
+  try{
+
+
+    await AsyncStorage.removeItem(key);
+
+
+
+  }
+
+  catch(error){
+
+
+    console.log(
+
+      "Erreur suppression :",
+
+      error
+
+    );
+
+
+  }
+
+
+};

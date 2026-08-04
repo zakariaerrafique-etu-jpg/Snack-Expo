ecfd33bec35a8a1aafc08ed81c8d5f5b06e33257
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 02:00:53 +0200

    Add food categories with icons to categories.js

--- data/categories.js
+++ data/categories.js
@@ -1,1 +1,84 @@
 
+const categories = [
+
+  {
+    id: "1",
+    name: "Petit déjeuner",
+    icon: "🍳"
+  },
+
+
+  {
+    id: "2",
+    name: "Déjeuner",
+    icon: "🍛"
+  },
+
+
+  {
+    id: "3",
+    name: "Dîner",
+    icon: "🍽️"
+  },
+
+
+  {
+    id: "4",
+    name: "Desserts",
+    icon: "🍰"
+  },
+
+
+  {
+    id: "5",
+    name: "Pizza",
+    icon: "🍕"
+  },
+
+
+  {
+    id: "6",
+    name: "Marocain",
+    icon: "🇲🇦"
+  },
+
+
+  {
+    id: "7",
+    name: "Fast Food",
+    icon: "🍔"
+  },
+
+
+  {
+    id: "8",
+    name: "Végétarien",
+    icon: "🥗"
+  },
+
+
+  {
+    id: "9",
+    name: "Boissons",
+    icon: "🥤"
+  },
+
+
+  {
+    id: "10",
+    name: "Pâtisserie",
+    icon: "🧁"
+  },
+
+
+  {
+    id: "11",
+    name: "Mes aliments",
+    icon: "👩‍🍳"
+  }
+
+
+];
+
+
+export default categories;

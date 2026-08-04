7acdfc643670c5b74c2c4ce3921c3f762dadcd1b
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-08-04 02:03:50 +0200

    Add RecipeCard component for displaying recipes

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,1 +1,157 @@
 
+import React from "react";
+
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+
+import FavoriteButton from "./FavoriteButton";
+
+
+
+export default function RecipeCard({
+
+  recipe,
+
+  onPress,
+
+  isFavorite,
+
+  toggleFavorite
+
+}) {
+
+
+return (
+
+<TouchableOpacity
+
+style={styles.card}
+
+onPress={onPress}
+
+>
+
+
+<Image
+
+source={{
+uri:recipe.image
+}}
+
+style={styles.image}
+
+/>
+
+
+
+<View style={styles.info}>
+
+
+<Text style={styles.name}>
+
+{recipe.name}
+
+</Text>
+
+
+
+<Text style={styles.category}>
+
+{recipe.category}
+
+</Text>
+
+
+
+</View>
+
+
+
+<FavoriteButton
+
+isFavorite={isFavorite}
+
+onPress={()=>toggleFavorite(recipe)}
+
+/>
+
+
+
+</TouchableOpacity>
+
+);
+
+
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+
+card:{
+
+
+backgroundColor:"#fff",
+
+margin:10,
+
+borderRadius:15,
+
+overflow:"hidden",
+
+elevation:3
+
+},
+
+
+image:{
+
+
+width:"100%",
+
+height:180
+
+
+},
+
+
+info:{
+
+
+padding:12
+
+
+},
+
+
+name:{
+
+
+fontSize:18,
+
+fontWeight:"bold"
+
+
+},
+
+
+category:{
+
+
+color:"gray",
+
+marginTop:5
+
+
+}
+
+
+
+});
